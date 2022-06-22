7d87b3fb0725a524f59db652eb41ebe65a1310d5
DesiAPG <desireapgames@gmail.com>
2022-06-22 15:40:36 -0500

    Controller using User main class

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "user/{id}")
+    public User getUser(@PathVariable String id) {
+        User user = new User();
+        user.setName("Duvan");
+        user.setSurname("Marin");
+        user.setEmail("mail@duvan.com");
+        user.setPhone("3208856258");
+        user.setPassword("1234");
+        return user;
+    }
+
+    @RequestMapping(value = "user")
+    public User createUser() {
+        User user = new User();
+        user.setName("Duvan");
+        user.setSurname("Marin");
+        user.setEmail("mail@duvan.com");
+        user.setPhone("3208856258");
+        user.setPassword("1234");
+        return user;
+    }
+    @RequestMapping(value = "user12")
+    public User modifyUser() {
+        User user = new User();
+        user.setName("Duvan");
+        user.setSurname("Marin");
+        user.setEmail("mail@duvan.com");
+        user.setPhone("3208856258");
+        user.setPassword("1234");
+        return user;
+    }
+
+    @RequestMapping(value = "user1")
+    public User deleteUser() {
+        User user = new User();
+        user.setName("Duvan");
+        user.setSurname("Marin");
+        user.setEmail("mail@duvan.com");
+        user.setPhone("3208856258");
+        user.setPassword("1234");
+        return user;
+    }
+}
