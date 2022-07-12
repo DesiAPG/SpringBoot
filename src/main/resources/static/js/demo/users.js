23611235aef2447280b56897a4e5f43e963ad1a0
DesiAPG <desireapgames@gmail.com>
2022-07-12 10:45:03 -0500

    Initial commit

---
+++ src/main/resources/static/js/demo/users.js
@@ -0,0 +1,20 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+    loadUsers();
+    $('#users').DataTable();
+});
+
+async function loadUsers(){
+    const request = await fetch("/user/10",
+        {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+
+    const users = await request.json()
+
+    console.log(users)
+}
